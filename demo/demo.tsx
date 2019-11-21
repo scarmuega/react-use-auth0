cb609e43e9738d21950cbb19f3225a9d7b7f8715
Santiago Carmuega <santiago@carmuega.me>
2019-11-21 15:35:13 -0300

    Migrate to new auth0 SPA sdk

--- demo/demo.tsx
+++ demo/demo.tsx
@@ -6,30 +6,35 @@ function PrivateData() {
     const auth = useAuth0();
     return (
         <div>
-            <span>your id token is: {auth.idToken}</span>
-            <button onClick={auth.logout}>logout</button>
+            <span>your id token is: {JSON.stringify(auth.user)}</span>
+            <button onClick={() => auth.client ? auth.client.logout() : alert('not')}>logout</button>
         </div>
     );
 }
 
 function LoginButton() {
     const auth = useAuth0();
-    return <button onClick={auth.login}>login!</button>
+    return <button onClick={auth.loginWithPopup}>login!</button>
 }
 
 function App() {
     const auth = useAuth0();
-    return auth.isAuthenticated() ? <PrivateData /> : <LoginButton />
+    return auth.isAuthenticated ? <PrivateData /> : <LoginButton />
 }
+
+// A function that routes the user to the right place
+// after login
+const onRedirectCallback = (appState: any) => {
+  window.location.pathname = "/"
+};
 
 ReactDOM.render(
     <Auth0Setup
         domain="my-tenant.auth0.com"
-        clientID="client-id-data-provided-by-auth0"
+        client_id="client-id-data-provided-by-auth0"
         redirectUri="http://localhost:3001"
-        responseType="token id_token"
+        onRedirectCallback={onRedirectCallback}
         scope="openid"
-        sessionStorage={true}
     >
         <App />
     </Auth0Setup>,
--- index.tsx
+++ index.tsx
@@ -1,95 +1,98 @@
-import React, { useState, useEffect, useRef, useContext, PropsWithChildren } from 'react';
-import auth0 from 'auth0-js';
+import React, { useState, useEffect, useContext, PropsWithChildren } from "react";
+import createAuth0Client from "@auth0/auth0-spa-js";
+import Auth0Client from "@auth0/auth0-spa-js/dist/typings/Auth0Client";
 
-export interface UseAuth0ContextValue {
-    login: () => void;
-    logout: () => void;
-    renew: () => void;
-    isAuthenticated: () => boolean;
-    accessToken: string | null;
-    idToken: string | null;
+const DEFAULT_REDIRECT_CALLBACK = () => window.history.replaceState({}, document.title, window.location.pathname);
+
+export interface Auth0Renders {
+    isAuthenticated: boolean;
+    user: any | null;
+    loading: boolean;
+    popupOpen: boolean;
+    client?: Auth0Client;
+    loginWithPopup?: () => void;
+    handleRedirectCallback?: () => void;
 }
 
-export interface Auth0ProviderProps {
-  domain: string;
-  clientID: string;
-  redirectUri: string;
-  responseType: string;
-  scope: string;
-  audience?: string;
-  sessionStorage: boolean | false;
+const Auth0Context = React.createContext<Auth0Renders>({
+    isAuthenticated: false,
+    user: null,
+    loading: false,
+    popupOpen: false,
+});
+
+export const useAuth0 = () => useContext(Auth0Context);
+
+export interface Auth0ProviderProps extends Auth0ClientOptions {
+    onRedirectCallback: (appState: any) => void;
 }
 
-type OnAuthChangeCallback = (result: auth0.Auth0DecodedHash | null) => void;
+export const Auth0Setup = (props: PropsWithChildren<Auth0ProviderProps>) => {
+  const [isAuthenticated, setIsAuthenticated] = useState();
+  const [user, setUser] = useState();
+  const [auth0Client, setAuth0] = useState();
+  const [loading, setLoading] = useState(true);
+  const [popupOpen, setPopupOpen] = useState(false);
 
-type MaybeDecodedHash = auth0.Auth0DecodedHash | null | undefined;
+  useEffect(() => {
+    const initAuth0 = async () => {
+      const auth0FromHook = await createAuth0Client(props);
+      setAuth0(auth0FromHook);
 
-const UninitializedAuthValue: UseAuth0ContextValue = {
-  login: () => console.warn("setup required"),
-  logout: () => console.warn("setup required"),
-  renew: () => console.warn("setup required"),
-  isAuthenticated: () => false,
-  accessToken: null,
-  idToken: null,
-};
+      if (window.location.search.includes("code=")) {
+        const { appState } = await auth0FromHook.handleRedirectCallback();
+        props.onRedirectCallback(appState);
+      }
 
-const UseAuth0Context = React.createContext<UseAuth0ContextValue>(UninitializedAuthValue);
+      const isAuthenticated = await auth0FromHook.isAuthenticated();
 
-function trySetAuthResultFromHash(webAuth: auth0.WebAuth, onResult: OnAuthChangeCallback) {
-  console.log("trying to parse hash");
-  webAuth.parseHash((err, authResult) => {
-    if (authResult && authResult.accessToken && authResult.idToken) {
-      onResult(authResult);
-    } else if (err) {
-      console.log(err);
-      onResult(null);
+      setIsAuthenticated(isAuthenticated);
+
+      if (isAuthenticated) {
+        const user = await auth0FromHook.getUser();
+        setUser(user);
+      }
+
+      setLoading(false);
+    };
+    initAuth0();
+    // eslint-disable-next-line
+  }, []);
+
+  const loginWithPopup = async (params = {}) => {
+    setPopupOpen(true);
+    try {
+      await auth0Client.loginWithPopup(params);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setPopupOpen(false);
     }
-  });
-}
+    const user = await auth0Client.getUser();
+    setUser(user);
+    setIsAuthenticated(true);
+  };
 
-function setSessionStorage(authResult: MaybeDecodedHash) {
-  authResult && authResult.accessToken && window.sessionStorage.setItem('accessToken', authResult.accessToken);
-  authResult && authResult.idToken && window.sessionStorage.setItem('idToken', authResult.idToken);
-}
-
-function clearSessionStorage() {
-  window.sessionStorage.removeItem('accessToken')
-  window.sessionStorage.removeItem('idToken')
-}
-
-function executLogout(webAuth: auth0.WebAuth, onAuthChange: OnAuthChangeCallback) {
-  webAuth.logout({ returnTo: window.location.origin });
-  clearSessionStorage();
-  onAuthChange(null);
-}
-
-function buildContextValueFromAuthResult(webAuth: auth0.WebAuth, result: MaybeDecodedHash, onAuthChange: OnAuthChangeCallback): UseAuth0ContextValue {
-  return {
-    login: () => webAuth.authorize(),
-    logout: () => executLogout(webAuth, onAuthChange),
-    renew: () => console.log("renew"),
-    isAuthenticated: () => ((!!result && !!result.accessToken && !!result.idToken) || (!!window.sessionStorage.getItem('accessToken') && !!window.sessionStorage.getItem('idToken'))),
-    accessToken: result && result.accessToken || window.sessionStorage.getItem('accessToken') || null,
-    idToken: result && result.idToken || window.sessionStorage.getItem('idToken') || null,
-  }
-}
-
-export function Auth0Setup(props: PropsWithChildren<Auth0ProviderProps>) {
-  const webAuth = useRef(new auth0.WebAuth({...props}));
-
-  const [authResult, setAuthResult] = useState<MaybeDecodedHash>(null);
-
-  useEffect(() => trySetAuthResultFromHash(webAuth.current, setAuthResult), []);
-
-  if (props.sessionStorage) {
-    setSessionStorage(authResult);
-  }
-
-  const output = buildContextValueFromAuthResult(webAuth.current, authResult, setAuthResult);
-
-  return <UseAuth0Context.Provider value={output}>{props.children}</UseAuth0Context.Provider>
-}
-
-export function useAuth0() {
-  return useContext(UseAuth0Context);
-}
+  const handleRedirectCallback = async () => {
+    setLoading(true);
+    await auth0Client.handleRedirectCallback();
+    const user = await auth0Client.getUser();
+    setLoading(false);
+    setIsAuthenticated(true);
+    setUser(user);
+  };
+  return (
+    <Auth0Context.Provider
+      value={{
+        isAuthenticated,
+        user,
+        loading,
+        popupOpen,
+        loginWithPopup,
+        handleRedirectCallback,
+      }}
+    >
+      {props.children}
+    </Auth0Context.Provider>
+  );
+};
